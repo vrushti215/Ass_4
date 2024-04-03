88ea2d4241ad5c4eb0214cc8fda4b2411bb77f22
vrushti215 <124078208+vrushti215@users.noreply.github.com>
2024-04-03 11:51:16 -0400

    The models has been created

--- vshah5494_src/ass4/settings.py
+++ vshah5494_src/ass4/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = {
+STATICFILES_DIRS = [
     BASE_DIR / 'static',
     BASE_DIR / 'posts' / 'static',
-    BASE_DIR / 'profiles' / 'static',
-}
+    BASE_DIR / 'profiles' / 'static'
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
