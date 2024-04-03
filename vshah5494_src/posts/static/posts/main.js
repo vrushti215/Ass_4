671cafd395f92220c8ca40eef9699dbde893f03e
vrushti215 <124078208+vrushti215@users.noreply.github.com>
2024-04-03 15:40:19 -0400

    Completed first DOM manipulation and ajax call

--- vshah5494_src/posts/static/posts/main.js
+++ vshah5494_src/posts/static/posts/main.js
@@ -1,1 +1,16 @@
 console.log('hello world')
+
+const helloWorldBox = document.getElementById('hello-world')
+
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success:  function(response) {
+        console.log('success',response.text)
+        helloWorldBox.textContent= response.text
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
