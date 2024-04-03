9c6499a2dfe3cfc20ee1029557ce613975238b7b
vrushti215 <124078208+vrushti215@users.noreply.github.com>
2024-04-03 18:25:10 -0400

    Post cards has been created

--- vshah5494_src/posts/static/posts/main.js
+++ vshah5494_src/posts/static/posts/main.js
@@ -27,7 +27,22 @@ $.ajax({
             console.log(data)
             data.forEach(el => {
                 postsBox.innerHTML += `
-                    ${el.title} - <b>${el.body}</b><br>
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <h5 class="card-title">${el.title}</h5>
+                            <p class="card-text">${el.body}</p>
+                            </div>
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-1">
+                                        <a href="#" class="btn btn-primary">Details</a>
+                                    </div>
+                                    <div class="col-1">
+                                        <a href="#" class="btn btn-primary">Like</a>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
                 `
             });
         }, 100)
