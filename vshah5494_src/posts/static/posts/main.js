ccce3e76154bf7c758df73777c15e51fe7644b21
vrushti215 <124078208+vrushti215@users.noreply.github.com>
2024-04-03 22:58:10 -0400

    Completed loading more posts using with a button click

--- vshah5494_src/posts/static/posts/main.js
+++ vshah5494_src/posts/static/posts/main.js
@@ -1,54 +1,63 @@
 console.log('hello world')
 
-const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
 const spinnerBox = document.getElementById('spinner-box')
+const loadBtn = document.getElementById('load-btn')
+const endbox = document.getElementById('end-box')
 
-$.ajax({
-    type: 'GET',
-    url: '/hello-world/',
-    success:  function(response) {
-        console.log('success',response.text)
-        helloWorldBox.textContent= response.text
-    },
-    error: function(error){
-        console.log('error', error)
-    }
-})
+let visible = 3
 
-$.ajax({
-    type: 'GET',
-    url: '/data/',
-    success: function(response){
-        console.log(response)
-        const data = response.data
-        setTimeout(()=>{
-            spinnerBox.classList.add('not-visible')
-            console.log(data)
-            data.forEach(el => {
-                postsBox.innerHTML += `
-                    <div class="card mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title">${el.title}</h5>
-                            <p class="card-text">${el.body}</p>
-                            </div>
-                            <div class="card-footer">
-                                <div class="row">
-                                    <div class="col-1">
-                                        <a href="#" class="btn btn-primary">Details</a>
-                                    </div>
-                                    <div class="col-1">
-                                        <a href="#" class="btn btn-primary">Like</a>
+const getData =() => {
+    $.ajax({
+        type: 'GET',
+        url: `/data/${visible}/`,
+        success: function(response){
+            console.log(response)
+            const data = response.data
+            setTimeout(()=>{
+                spinnerBox.classList.add('not-visible')
+                console.log(data)
+                data.forEach(el => {
+                    postsBox.innerHTML += `
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title">${el.title}</h5>
+                                <p class="card-text">${el.body}</p>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="row">
+                                        <div class="col-1">
+                                            <a href="#" class="btn btn-primary">Details</a>
+                                        </div>
+                                        <div class="col-1">
+                                            <a href="#" class="btn btn-primary">Like</a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                    </div>
-                `
-            });
-        }, 100)
-    },
-    error: function(error){
-        console.log(error)
-    }
+                        </div>
+                    `
+                });
+            }, 100)
+            console.log(response.size)
+            if (response.size === 0) {
+                endbox.textContent = 'No posts added yet...'
+            }
+            else if (response.size <= visible) {
+                loadBtn.classList.add('not-visible')
+                endbox.textContent = 'No more posts to load...'
+            }
+        },
+        error: function(error){
+            console.log(error)
+        }
+    
+    })
+}
 
+loadBtn.addEventListener('click', ()=>{
+    spinnerBox.classList.remove('not-visible')
+    visible += 3
+    getData()
 })
+
+getData()
