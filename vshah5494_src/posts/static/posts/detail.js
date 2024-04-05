19f7dfd7e9683a1cedb73f82e4d911285ba96148
vrushti215 <124078208+vrushti215@users.noreply.github.com>
2024-04-05 16:41:12 -0400

    Deleting post with ajax and working with local storage

--- vshah5494_src/posts/static/posts/detail.js
+++ vshah5494_src/posts/static/posts/detail.js
@@ -1,4 +1,5 @@
 console.log('hello world detail')
+
 const postBox  = document.getElementById('post-box')
 const alertBox = document.getElementById('alert-box')
 const backBtn = document.getElementById('back-btn')
@@ -86,5 +87,24 @@ updateForm.addEventListener('submit', e=>{
             console.log(error)
         }
 
+    })
+})
+
+deleteForm.addEventListener('submit', e =>{
+    e.preventDefault()
+
+    $.ajax({
+        type: 'POST',
+        url: deleteUrl,
+        data: {
+            'csrfmiddlewaretoken': csrf[0].value,
+        },
+        success: function(response){
+            window.location.href = window.location.origin
+            localStorage.setItem('title',titleInput.value)
+        },
+        error:  function(error){
+            console.log(error)
+        }
     })
 })
--- vshah5494_src/posts/urls.py
+++ vshah5494_src/posts/urls.py
@@ -18,7 +18,6 @@ urlpatterns = [
     path ('<pk>/update/',update_post, name='post-update'),
     path ('<pk>/delete/',delete_post, name='post-delete'),
 
-
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
     path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
    ]
