0bd5308de3fd6de340b1c64806eac7b76154e362
vrushti215 <124078208+vrushti215@users.noreply.github.com>
2024-04-05 18:25:01 -0400

    Completed the creation of view for image uploading

--- vshah5494_src/posts/views.py
+++ vshah5494_src/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .forms import PostForm
 from profiles.models import Profile
 
@@ -94,4 +94,12 @@ def delete_post(request, pk):
     obj = Post.objects.get(pk=pk)
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
         obj.delete()
-    return JsonResponse({})
+        return JsonResponse({})
+
+def image_upload_view(request):
+    if request.method='POST'
+    img = request.FILES.get('file')
+    new_post_id = request.POST.get('new_post_id')
+    post = Post.object.get(id=new_post_id)
+    Photo.objects.create(image=img, post=post)
+return HttpResponse()
