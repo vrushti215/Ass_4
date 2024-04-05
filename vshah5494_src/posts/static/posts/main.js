6a950e525d391bb40e3e26df654431ccf2d5596b
vrushti215 <124078208+vrushti215@users.noreply.github.com>
2024-04-05 19:34:31 -0400

    Created the user profile view

--- vshah5494_src/posts/static/posts/main.js
+++ vshah5494_src/posts/static/posts/main.js
@@ -180,6 +180,8 @@ closeBtns.forEach(btn=> btn.addEventListener('click', () => {
     if (!dropzone.classList.contains('not-visible')) {
         dropzone.classList.add('not-visible')
     }
+    const myDropzone = Dropzone.forElement("#my-dropzone")
+    myDropzone.removeAllFiles(true)
 }))
 
 Dropzone.autoDiscover = false
@@ -191,9 +193,9 @@ const myDropzone = new Dropzone('#my-dropzone', {
             formData.append('new_post_id', newPostId)
         })
     },
-    maxFiles: 5,
+    maxFiles: 3,
     maxFilesize: 4,
-    acceptedFiles: '.png, .jpg, .jpeg'
+    acceptedFiles: '.png .jpeg .jpg'
 })
 
 getData()
