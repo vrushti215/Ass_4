3c660ad55d74ef3588b13a3077a9f4087c4b0362
vrushti215 <124078208+vrushti215@users.noreply.github.com>
2024-04-05 15:42:42 -0400

    Adding modals for update and delete post

--- vshah5494_src/posts/static/posts/detail.js
+++ vshah5494_src/posts/static/posts/detail.js
@@ -6,6 +6,9 @@ const deleteBtn= document.getElementById('delete-btn')
 const url = window.location.href + "data/"
 const spinnerBox = document.getElementById('spinner-box')
 
+const titleInput = document.getElementById('id_title')
+const bodyInput = document.getElementById('id_body')
+
 backBtn.addEventListener('click', () => {
     history.back()
 })
@@ -36,6 +39,9 @@ $.ajax({
 
         postBox.appendChild(titleEl)
         postBox.appendChild(bodyEl)
+
+        titleInput.value = data.title
+        bodyInput.value = data.body
 
         spinnerBox.classList.add('not-visible')
     },
