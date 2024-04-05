0b37616959f13d98a35b76200ab170b8ca8afdcc
vrushti215 <124078208+vrushti215@users.noreply.github.com>
2024-04-05 14:29:39 -0400

    Completed part 1 and 2 of post detail page

--- vshah5494_src/posts/urls.py
+++ vshah5494_src/posts/urls.py
@@ -4,6 +4,7 @@ from .views import (
     load_post_data_view,
     like_unlike_post,
     post_detail,
+    post_detail_data_view
 )
 
 app_name = 'posts'
@@ -14,5 +15,5 @@ urlpatterns = [
     path ('<pk>/',post_detail, name='post-detail'),
 
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
-
+    path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
    ]
