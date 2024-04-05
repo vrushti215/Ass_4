fd40792bcf81ffe5f90581a8a7e4317ac170c2d9
vrushti215 <124078208+vrushti215@users.noreply.github.com>
2024-04-05 15:00:25 -0400

    Completed part 3 and 4 of post detail page

--- vshah5494_src/posts/static/posts/detail.js
+++ vshah5494_src/posts/static/posts/detail.js
@@ -1,6 +1,8 @@
 console.log('hello world detail')
-
+const postBox  = document.getElementById('post-box');
 const backBtn = document.getElementById('back-btn')
+const updateBtn = document.getElementById('update-btn')
+const deleteBtn= document.getElementById('delete-btn')
 const url = window.location.href + "data/"
 const spinnerBox = document.getElementById('spinner-box')
 
@@ -12,7 +14,29 @@ $.ajax({
     type: 'GET',
     url: url,
     success: function(response){
-        console.log(response),
+        console.log(response)
+        const data = response.data
+
+        if(data.logged_in != data.author){
+            console.log('different')
+        } else{
+            console.log('the same')
+            updateBtn.classList.remove('not-visible')
+            deleteBtn.classList.remove('not-visible')
+        }
+
+        const titleEl = document.createElement('h3')
+        titleEl.setAttribute('class','mt-3')
+
+        const bodyEl = document.createElement('p')
+        bodyEl.setAttribute('class','mt-1')
+
+        titleEl.textContent = data.title
+        bodyEl.textContent = data.body
+
+        postBox.appendChild(titleEl)
+        postBox.appendChild(bodyEl)
+
         spinnerBox.classList.add('not-visible')
     },
     error: function(error){
