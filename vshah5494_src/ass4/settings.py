75d8248999974d96bb3542774378687b34dc471c
vrushti215 <124078208+vrushti215@users.noreply.github.com>
2024-04-06 20:16:50 -0400

    Adding local settings import for deployement

--- vshah5494_src/ass4/settings.py
+++ vshah5494_src/ass4/settings.py
@@ -138,3 +138,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *  
+except ImportError:
+        pass  # It's okay if there
